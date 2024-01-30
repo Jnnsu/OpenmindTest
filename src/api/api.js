6aa727750337fe551bbbebec0e9d05f5837f8622
Jnnsu <jspp120@gmail.com>
2024-01-30 13:44:58 +0900

    sdsd

--- src/api/api.js
+++ src/api/api.js
@@ -158,7 +158,7 @@ export async function deleteQuestion(questionId) {
 export const getWeatherData = async (lat, lon) => {
     try {
         const response = await fetch(
-            `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&lang=kr&units=metric`,
+            `/api/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&lang=kr&units=metric`,
             {
                 method: 'GET',
                 headers: {
